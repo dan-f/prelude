91476fa8d4c726e442def85aac3ad9563cba83e7
Daniel Friedman <dfriedman58@gmail.com>
2014-10-19 21:23:58 -0400

    Add module for prelude-specific customizations

--- personal/danf-global.el
+++ personal/danf-global.el
@@ -4,8 +4,6 @@
     ;; (setq mouse-wheel-progressive-speed t)
     (setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))))
 
-(delete* 'lines-tail whitespace-style) ; don't highlight lines longer than whitespace-line-column
-
 
 ;; keybindings
 (global-set-key (kbd "C-c ;") 'dan-f/comment-or-uncomment-line-or-region)
---
+++ personal/danf-prelude.el
@@ -0,0 +1,5 @@
+(setq prelude-guru nil)
+
+(delete* 'lines-tail whitespace-style) ; don't highlight lines longer than whitespace-line-column
+
+(provide 'danf-prelude)
