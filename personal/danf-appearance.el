52ac968a822280ae9d5d7d7e69c3322ad7f620bb
Daniel Friedman <dfriedman58@gmail.com>
2014-10-21 23:17:30 -0400

    Use cyberpunk theme.

--- personal/danf-appearance.el
+++ personal/danf-appearance.el
@@ -1,12 +1,14 @@
 (scroll-bar-mode -1)
 (set-frame-font "Source Code Pro 13")
 
-(prelude-require-packages '(flatland-theme
+(prelude-require-packages '(cyberpunk-theme
+                            flatland-theme
                             flatui-theme
                             grandshell-theme
                             gruvbox-theme))
 
 (setq custom-safe-themes 't)
 (disable-theme 'zenburn)
+(load-theme 'cyberpunk)
 
 (provide 'danf-appearance)
